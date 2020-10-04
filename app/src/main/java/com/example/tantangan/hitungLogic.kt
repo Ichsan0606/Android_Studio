689b3887b50fe9de1b12d2efe91be17e9fbe4b57
ichsan0606 <ichs4nm4ulana@gmail.com>
2020-10-04 07:12:59 +0700

    first commit

---
+++ app/src/main/java/com/example/tantangan/hitungLogic.kt
@@ -0,0 +1,19 @@
+package com.example.tantangan
+
+class hitungLogic {
+    var result=0.0
+
+    fun hitungKali(h1:Double, h2:Double, h3:Double) : Double {
+        this.result = h1 * h2 * h3
+        return h1*h2*h3
+    }
+    fun hitungKurang(h1:Double, h2:Double, h3:Double) : Double {
+        this.result = h1 - h2 - h3
+        return h1-h2-h3
+    }
+
+    fun hitungBagi(h1:Double, h2:Double, h3:Double) : Double {
+        this.result = h1 % h2 % h3
+        return h1%h2%h3
+    }
+}
